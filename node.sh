d96a9944a706ca030de255bc4fd6f40b6e2c38db
ctrprabhu <ctrprabhu@gmail.com>
2024-07-02 22:08:59 +0545

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,4 +1,27 @@
-apt-get update -y
-apt-get install curl wget -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+#!/bin/bash
 
+# Update and install necessary packages
+sudo apt update
+sudo apt install -y curl
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# Load nvm into the current shell session
+export NVM_DIR="$HOME/.nvm"
+# This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# This loads nvm bash_completion
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+
+# Install Node.js version 16
+#nvm install 16
+
+# Set Node.js version 16 as the default version
+#nvm use 16
+#nvm alias default 16
+
+# Verify the installation
+#node -v
+#npm -v
