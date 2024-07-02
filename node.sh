b12ed169353ceb5b847a58288d1222536696e5b9
ctrprabhu <ctrprabhu@gmail.com>
2024-07-02 22:37:59 +0545

    Update node.sh

--- node.sh
+++ node.sh
@@ -7,6 +7,9 @@ sudo apt install -y curl
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
+# Source .bashrc to load nvm
+source ~/.bashrc
+
 # Load nvm into the current shell session
 export NVM_DIR="$HOME/.nvm"
 # This loads nvm
@@ -14,14 +17,13 @@ export NVM_DIR="$HOME/.nvm"
 # This loads nvm bash_completion
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-echo $NVM_DIR
 # Install Node.js version 16
-#nvm install 16
+nvm install 16
 
 # Set Node.js version 16 as the default version
-#nvm use 16
-#nvm alias default 16
+nvm use 16
+nvm alias default 16
 
 # Verify the installation
-#node -v
-#npm -v
+node -v
+npm -v
