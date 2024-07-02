708aa56df5ef65c62d575dae3906fdef73ccd49f
ctrprabhu <ctrprabhu@gmail.com>
2024-07-02 21:45:06 +0545

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,5 @@
 apt-get update -y
-apt-get install curl -y
+apt-get install curl wget -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source /root/.bashrc
 echo $NVM_DIR
