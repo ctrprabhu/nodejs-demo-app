865e548b7f20565bf27475db409fc8b44be3b6a7
ctrprabhu <ctrprabhu@gmail.com>
2024-11-13 22:39:19 +0545

    Update node1.sh
    sdf

--- node1.sh
+++ node1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Update and install necessary packages
+. /home/ubuntu/.bashrc
 date
 /home/ubuntu/.nvm/versions/node/v20.16.0/bin/node -v
 /home/ubuntu/.nvm/versions/node/v20.16.0/bin/npm -v
