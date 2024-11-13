fdb4487fa1b04d29d294571afc5d059c5337c294
ctrprabhu <ctrprabhu@gmail.com>
2024-11-13 22:51:05 +0545

    Update node1.sh
    sdfsdfsdf

--- node1.sh
+++ node1.sh
@@ -2,7 +2,7 @@
 PATH="/home/ubuntu/.nvm/versions/node/v20.16.0/bin:$PATH"
 . /home/ubuntu/.bashrc
 date
-/home/ubuntu/techhaus-website/
+cd /home/ubuntu/techhaus-website/
 node -v
 npm -v
 npm install
